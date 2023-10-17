274c5e91406ea7c0349e46ef2b9306d743105d54
Elkin Barreto <elkin.rojas@northumbria.ac.uk>
2023-10-17 07:25:08 +0100

    Commit

--- cartype/statistical.py
+++ cartype/statistical.py
@@ -1,15 +1,37 @@
-#Importing files
+#1. Notebook Preparation
+
+##1.1 importing libraries
 import numpy as np
 import pandas as pd
+
+##1.2 importing files
 df = pd.read_csv('cartype CSV.csv')
 data = df
 
-#Preaparation and Cleaning Dataset
+#2. Cleaning Dataset
+##2.1 Missing Values / Erros
+data.info()
 
-#Find Missing Values / Erros
-#data.info()
-#No null numbers found
-
-#Find Duplicates Values
+##2.2  Duplicates Values
 print(data.describe())
+
+##2.3  Outliers
+
+
+#3  Data Analytical Method
+
+
+##3.1  Descriptive Statistic Analysis
+
+
+##3.2  Test of Normality
+
+
+##3.3  Pearson Linear Correlation Coefficient
+
+
+##3.4  Chi-Square Test
+
+
+##3.5  Graphical Analysis of Variables Associated
 
