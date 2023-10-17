1cb4ae79e03bfc47e381d7458c35a50ac93cc464
Elkin Barreto <elkin.rojas@northumbria.ac.uk>
2023-10-17 20:18:34 +0100

    Commit

--- cartype/statistical_analysis.py
+++ cartype/statistical_analysis.py
@@ -4,7 +4,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sb
+import seaborn as sns
 
 ##1.2 importing files
 df = pd.read_csv('cartype CSV.csv')
@@ -20,9 +20,37 @@ data.drop_duplicates()
 data.describe()
 
 ##2.3  Outliers
-fig = plt.figure(figsize=(10, 7))
-plt.boxplot(data['Combined FE'])
+#Boxplot to visualize data
+fig = plt.figure(figsize=(11, 7))
+sns.boxplot(y=data['Combined FE'], legend="full")
 plt.show()
+
+#Function to detect Outliers in Combined FE Column
+outliers = []
+def detect_outliers_iqr(data_sample):
+    data_sample = sorted(data_sample)
+    q1 = np.quantile(data_sample, 0.25)
+    q3 = np.quantile(data_sample, 0.75)
+    IQR = q3-q1
+    lwr_bound = q1-(1.5*IQR)
+    upr_bound = q3+(1.5*IQR)
+    for i in data_sample:
+        if i < lwr_bound or i> upr_bound:
+            outliers.append(i)
+    return outliers
+sample_outliers = detect_outliers_iqr(data['Combined FE'])
+print(sample_outliers)
+
+#As suggested technique in this case. The Outliers will be replaced by the mean rather than delete them
+median_FE = np.median(data['Combined FE'])
+print(median_FE)
+for i in sample_outliers:
+    c = np.where(data['Combined FE'] == i, 23, data['Combined FE'])
+
+fig = plt.figure(figsize=(11, 7))
+sns.boxplot(y=c, legend="full")
+plt.show()
+
 
 #3  Data Analytical Method
 
@@ -41,3 +69,6 @@ plt.show()
 
 ##3.5  Graphical Analysis of Variables Associated
 
+
+#Export Data
+data.to_excel('C:\Other test\cartype\Data.Pandas.xlsx')
