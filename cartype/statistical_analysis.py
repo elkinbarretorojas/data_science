66b32ec0a85ec4927a397f938c8da8ff9dddce87
Elkin Barreto <elkin.rojas@northumbria.ac.uk>
2023-10-19 10:15:44 +0100

    Commit

--- cartype/statistical_analysis.py
+++ cartype/statistical_analysis.py
@@ -19,14 +19,21 @@ data = df
 
 #2. Cleaning Dataset
 ##2.1 Missing Values / Erros
-#data.info()
+data.info()
 
 ##2.2  Duplicates Values
-duplicates_values = data.duplicated()
-data.drop_duplicates()
-print(data.describe())
+duplicate_rows_df = data[data.duplicated()]
+print("number of duplicate rows: ", duplicate_rows_df.shape)
+data = data.drop_duplicates()
+data.count()
 
-##2.3  Outliers
+##2.3 Dropping irrelevant Columns
+data = data.drop(['Mfr Name', 'Model Year'], axis=1)
+
+##2.6  Dropping Null Values
+print(data.isnull().sum())
+
+##2.5  Outliers
 #Boxplot to visualize data
 fig = plt.figure(figsize=(11, 7))
 sns.boxplot(y=data['Combined FE'], legend="full")
@@ -34,33 +41,32 @@ sns.boxplot(y=data['Combined FE'], legend="full")
 
 #Function to detect Outliers in Combined FE Column
 outliers = []
-def detect_outliers_iqr(data_sample):
-    data_sample = sorted(data_sample)
-    q1 = np.quantile(data_sample, 0.25)
-    q3 = np.quantile(data_sample, 0.75)
-    IQR = q3-q1
-    lwr_bound = q1-(1.5*IQR)
-    upr_bound = q3+(1.5*IQR)
-    for i in data_sample:
-        if i < lwr_bound or i> upr_bound:
-            outliers.append(i)
-    return outliers
-sample_outliers = detect_outliers_iqr(data['Combined FE'])
-print(sample_outliers)
+data['Combined FE'] = sorted(data['Combined FE'])
+q1 = np.quantile(data['Combined FE'], 0.25)
+q3 = np.quantile(data['Combined FE'], 0.75)
+IQR = q3-q1
+lwr_bound = q1-(1.5*IQR)
+upr_bound = q3+(1.5*IQR)
+
+for i in data['Combined FE']:
+    if i < lwr_bound or i> upr_bound:
+        outliers.append(i)
+
+print(outliers)
 
 #As suggested technique in this case. The Outliers will be replaced by the mean rather than delete them
 median_FE = np.median(data['Combined FE'])
 print(median_FE)
-for i in sample_outliers:
-    c = np.where(data['Combined FE'] == i, 23, data['Combined FE'])
+for i in outliers:
+    data['Combined FE'] = np.where(data['Combined FE'] == i, 23, data['Combined FE'])
 
 fig = plt.figure(figsize=(11, 7))
-sns.boxplot(y=c, legend="full")
-#plt.show()
+sns.boxplot(y=data['Combined FE'], legend="full")
+plt.show()
 
 #3  Data Analytical Method
 ##3.1  Descriptive Statistic Analysis
-print(data.describe())
+print(data['Combined FE'].describe())
 
 ##3.2  Test of Normality
 ##Create a Histogram
@@ -82,15 +88,13 @@ print(kstest(data['Combined FE'], 'norm'))
 #Since the p-value is less than .05, we reject the null hypothesis of the Kolmogorov-Smirnov test. This means we have sufficient evidence to say that the sample data does not come from a normal distribution.
 
 ##3.3  Pearson Linear Correlation Coefficient
-data.info()
-combined_correlation = np.corrcoef(data['Combined FE'], data['Combined CO2'])
-print(combined_correlation)
-combined_engine = np.corrcoef(data['Combined FE'], data['Engine Displacement'])
-print(combined_engine)
-combined_cylinders = np.corrcoef(data['Combined FE'], data['# Cylinders'])
-print(combined_cylinders)
-combined_gears = np.corrcoef(data['Combined FE'], data['# Gears'])
-print(combined_gears)
+#Using Heatmap to analyse correlations
+
+plt.figure(figsize=(10,5))
+data_numerical = data.drop(['Division', 'Carline', 'Transmission', 'Air Aspiration Method', 'Transmission Description', 'Drive Desc', 'Carline Class Desc', 'Release Date'], axis=1)
+c= data_numerical.corr()
+sns.heatmap(c,cmap="BrBG",annot=True)
+plt.show()
 
 #Strong negative correlation between Combined FE with Engine Displacement, No of Cylinders, and Combined FE.
 #Weak negative correlation between Combined FE and No Gears.
@@ -98,7 +102,7 @@ print(combined_gears)
 #Weak positive correlation between Combined FE and No Gears
 
 ##3.4  Chi-Square Test
-
+#Using test for categorical data and find correlations
 result = chisquare(f_obs=data['Combined FE'])
 print(result)
 
